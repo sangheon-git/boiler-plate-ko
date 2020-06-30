57287da4ff05ebeed7fd61664085b9df50ae8029
sangheon <tkdgjs0415@gmail.com>
2020-06-30 14:57:35 +0900

    비밀 정보 보호

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const cors = require('cors')
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+const {User} = require("./models/User");
+
+
 const config = require("./config/key");
 
 // const mongoose = require("mongoose");
@@ -22,6 +25,35 @@ const connect = mongoose.connect(config.mongoURI,
   })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
+
+
+
+//application/x-www-form-urlcoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
+
+app.get('/',(req,res) => res.send('Hello World!~~안녕하세요 ~ 새해복 많이 받으세요'));
+
+
+app.post('/register',(req,res)=>{
+
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  
+  const user = new User(req.body)
+
+  user.save((err,userInfo)=>{
+    if(err){return res.json({success:false,err})
+    return res.status(200).json({
+      success:true
+    })
+    }
+  })
+})
+
+
+
 
 app.use(cors())
 
